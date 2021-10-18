75f7381a7f97b8fc3b14da606062281e8d7eb5fd
Wujun Zhou <w@ellieiris.com>
2021-10-18 14:35:18 -0400

    update obs socket for better error handling

--- workspaces/server/src/obs.ts
+++ workspaces/server/src/obs.ts
@@ -1,53 +1,78 @@
-import OBSWebSocket from "obs-websocket-js";
-import EventEmitter from "events";
-import { logger } from "./logger";
+import WebSocket from 'ws';
+import EventEmitter from 'events';
+import { logger } from './logger';
+import ObsWebSocket from 'obs-websocket-js';
+import { idText } from 'typescript';
+import { ObsEvent } from './obs-types';
 
+type SendType = InstanceType<typeof ObsWebSocket>['send'];
+type RequestEvent = {
+  'message-id': string;
+  status: 'ok' | 'error';
+  error?: string;
+};
 export class Obs {
-  obs = new OBSWebSocket();
+  ws: WebSocket | null = null;
   connnected = false;
   connecting = false;
   connectEvent = new EventEmitter();
-  currentScene = "";
+  currentScene = '';
   scenes: string[] = [];
 
   /**
    *
    */
-  constructor(public address: string, public name: string) {
-    this.obs.on("ConnectionOpened", () => {
-      logger.debug("obs ConnectionOpened", address);
-    });
-    this.obs.on("ConnectionClosed", () => {
-      logger.debug("obs ConnectionClosed", address);
-    });
-    this.obs.on("AuthenticationSuccess", () => {
-      logger.debug("obs AuthenticationSuccess", address);
-      this.refreshScenes().catch((error) => {
-        logger.warn("refreshScenes Error", error);
-      });
-    });
-    this.obs.on("AuthenticationFailure", () => {
-      logger.error("obs AuthenticationFailure", address);
-    });
-    this.obs.on("SwitchScenes", (data) => {
-      this.currentScene = data["scene-name"];
-    });
-    this.obs.on("ScenesChanged", (data) => {
-      this.scenes = data.scenes.map((a) => a.name);
-    });
-  }
-  async connect(): Promise<void> {
-    this.connecting = true;
-    try {
-      await this.obs.connect({ address: this.address, password: "worship" });
-      this.connnected = true;
-      this.connecting = false;
-      this.connectEvent.emit("sucessful");
-    } catch (error) {
-      this.connnected = false;
-      this.connecting = false;
-      this.connectEvent.emit("error", error);
+  constructor(public address: string, public name: string) {}
+
+  handleUpdateEvent(event: ObsEvent) {
+    switch (event['update-event']) {
+      case 'ScenesChanged':
+        this.scenes = event.scenes.map((a) => a.name);
+        break;
+      case 'SwitchScenes':
+        this.currentScene = event['scene-name'];
+        break;
     }
+  }
+  async connect(): Promise<WebSocket> {
+    return new Promise((resolve, reject) => {
+      if (this.connecting) {
+        this.ws?.once('open', resolve);
+        this.ws?.once('close', reject);
+      }
+      this.connecting = true;
+      const ws = new WebSocket(this.address, {
+        perMessageDeflate: false,
+      });
+      ws.once('open', () => {
+        this.connecting = false;
+        resolve(ws);
+      });
+      ws.once('error', (error) => {
+        this.connecting = false;
+
+        reject(error);
+      });
+      ws.onmessage = (event) => {
+        const response = JSON.parse(event.data.toString()) as
+          | RequestEvent
+          | {
+              'update-type': string;
+            };
+        if ('message-id' in response) {
+          const key = Number(response['message-id']);
+          const callback = this.listeners.get(key);
+          if (callback) {
+            callback(response);
+          }
+          this.listeners.delete(key);
+        } else if ('update-event' in response) {
+          this.handleUpdateEvent(response);
+        } else {
+          logger.info('Unknown event:', response);
+        }
+      };
+    });
   }
 
   async getScenes() {
@@ -59,60 +84,62 @@ export class Obs {
   }
 
   async refreshScenes() {
-    await this.checkConnection();
-    const scenes = await this.obs.send("GetSceneList");
-    this.currentScene = scenes["current-scene"];
+    const scenes = await this.send('GetSceneList');
+    this.currentScene = scenes['current-scene'];
     this.scenes = scenes.scenes.map((a) => a.name);
   }
 
   async setScrene(scene: string) {
-    await this.checkConnection();
-    this.obs.send("SetCurrentScene", { "scene-name": scene });
+    this.send('SetCurrentScene', { 'scene-name': scene });
   }
 
   async getScreenshot(scene: string, width: number) {
-    await this.checkConnection();
-    const result = await this.obs.send("TakeSourceScreenshot", {
+    const result = await this.send('TakeSourceScreenshot', {
       sourceName: scene,
       width,
-      embedPictureFormat: "png",
+      embedPictureFormat: 'png',
     });
     return result.img;
   }
 
   async openProjector(monitor: number) {
-    await this.checkConnection();
-    await this.obs.send("OpenProjector", { type: "Preview", monitor: monitor });
+    await this.send('OpenProjector', { type: 'Preview', monitor: monitor });
   }
 
-  async checkConnection() {
-    if (!this.connnected) {
-      if (!this.connecting) {
-        await this.connect();
-      } else {
-        await new Promise((resolve, reject) => {
-          let resolved = false;
-          this.connectEvent.once("sucessful", () => {
-            if (!resolved) {
-              resolved = true;
-              resolve(undefined);
-            }
-          });
-          this.connectEvent.once("error", (error) => {
-            if (!resolved) {
-              resolved = true;
-              reject(error);
-            }
-          });
-        });
-      }
+  async getConnection(): Promise<WebSocket> {
+    if (this.ws && this.ws.readyState == WebSocket.OPEN) {
+      return this.ws;
     }
+    return await this.connect();
   }
+
+  listeners = new Map<number, (event: RequestEvent) => void>();
+  msgId = 1;
+  send: SendType = async (requestType, args?) => {
+    const ws = await this.getConnection();
+    return new Promise((resolve, reject) => {
+      const msgId = this.msgId++;
+      const data = {
+        'request-type': requestType,
+        'message-id': msgId,
+        ...args,
+      };
+      ws.send(JSON.stringify(data));
+      this.listeners.set(msgId, (event) => {
+        if (event.status === 'error') {
+          reject(event.error);
+        } else {
+          // this is returned from the server
+          resolve(event as any);
+        }
+      });
+    });
+  };
 }
 
 export const obss = [
-  new Obs("localhost:4444", "main-projector"),
-  new Obs("localhost:4445", "zoom"),
-  new Obs("localhost:4446", "tv"),
-  new Obs("localhost:4447", "streaming"),
+  new Obs('localhost:4444', 'main-projector'),
+  new Obs('localhost:4445', 'zoom'),
+  new Obs('localhost:4446', 'tv'),
+  new Obs('localhost:4447', 'streaming'),
 ];
